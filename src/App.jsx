8cc8a3cfad8de94abab70a7c9c4b1be98cdd7085
Marion Penel <marionpenel@hotmail.fr>
2026-02-10 16:01:45 +0100

    Configuration des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/Home/index.jsx'
+import About from './pages/About/index.jsx'
+import Error from './pages/Error/index.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   )
 }
