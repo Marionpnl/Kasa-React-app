3053931eb1cc581768e7a8eaf28c61a08d2d9781
Marion Penel <marionpenel@hotmail.fr>
2026-02-16 13:58:31 +0100

    Création et intégration du composant Collapse + responsive design

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import arrow from "../../assets/arrow.png";
+
+import "./Collapse.scss";
+
+function Collapse({title, content}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapse">
+        <div className="collapse__header" >
+            <h2 className="collapse__title">{title}</h2>
+            <button className="collapse__button" onClick={toggleCollapse}>
+                <img src={arrow} alt="Chevron de la collapse" className= {`collapse__arrow ${isOpen ? 'rotate' : ''}`} />
+            </button>
+        </div>
+        <div className={`collapse__content ${isOpen ? 'show' : ''}`}>
+            {Array.isArray(content) ? (
+                <ul>
+                    {content.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>{content}</p>
+            )}
+        </div>
+    </div>
+  );
+}
+
+export default Collapse
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,12 +1,35 @@
 import Banner from "../../components/Banner";
 import imgAbout from "../../assets/img_about_banner.png";
+import Collapse from "../../components/Collapse";
+
+import "./About.scss";
 
 function About() {
   return (
-    <Banner 
-        image={imgAbout}
-        opacity={0.3}
-    />
+    <div className="about">
+      <Banner 
+          image={imgAbout}
+          opacity={0.3}
+      />
+      <div className="collapses">
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiés par nos équipes."
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Service"
+          content="La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
+        />
+        <Collapse
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
+      </div>
+    </div>
   )
 }
 
