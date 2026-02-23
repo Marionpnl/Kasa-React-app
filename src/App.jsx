632cf96c5256bd4614871b9e187d3b46b3ff10dd
Marion Penel <marionpenel@hotmail.fr>
2026-02-23 16:17:51 +0100

    Création composant AppRouter pour isoler les routes et modif App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home/index.jsx'
-import About from './pages/About/index.jsx'
-import Error from './pages/Error/index.jsx'
-import Layout from './layout/Layout/index.jsx'
-import Logement from './pages/Logement/index.jsx'
+import AppRouter from "./Router.jsx"
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/logement/:id" element={<Logement />} />
-          <Route path="/404" element={<Error />} />
-          <Route path="*" element={<Navigate to="/404" />} />
-        </Route>
-      </Routes>
+      <AppRouter />
     </>
   )
 }
---
+++ src/Router.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home/index.jsx'
+import About from './pages/About/index.jsx'
+import Error from './pages/Error/index.jsx'
+import Layout from './layout/Layout/index.jsx'
+import Logement from './pages/Logement/index.jsx'
+
+function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logement/:id" element={<Logement />} />
+        <Route path="/404" element={<Error />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default AppRouter
